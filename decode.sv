7f214fd90cef4d66b7d65c756c3eee3dfce67081
Michael J. Spencer <bigcheesegs@gmail.com>
2018-02-21 03:51:24 -0800

    Decode func12 and csr zimm.

--- decode.sv
+++ decode.sv
@@ -1,6 +1,6 @@
 module decode(input clk, input [31:0] inst, output [4:0] opcode, output [31:0] imm,
               output [4:0] rs1, output [4:0] rs2, output [4:0] rd, output [2:0] func3,
-              output [6:0] func7, output invalid);
+              output [6:0] func7, output [11:0] func12, output invalid);
   wire [31:0] immI = {{21{inst[31]}}, inst[30:20]};
 
   wire [31:0] immS = {{21{inst[31]}}, inst[30:25], inst[11:7]};
@@ -11,12 +11,15 @@ module decode(input clk, input [31:0] inst, output [4:0] opcode, output [31:0] i
 
   wire [31:0] immJ = {{21{inst[31]}}, inst[19:12], inst[20], inst[30:21], 1'b0};
 
+  wire [31:0] immZ = {27'b0, rs1};
+
   assign rs2 = inst[24:20];
   assign rs1 = inst[19:15];
   assign rd = inst[11:7];
 
   assign func3 = inst[14:12];
   assign func7 = inst[31:25];
+  assign func12 = inst[31:20];
 
   assign opcode = inst[6:2];
 
@@ -26,12 +29,14 @@ module decode(input clk, input [31:0] inst, output [4:0] opcode, output [31:0] i
   wire B = opcode == 5'b11000 ? 1 : 0;
   wire U = (opcode == 5'b01101 || opcode == 5'b00101) ? 1 : 0;
   wire J = opcode == 5'b11011 ? 1 : 0;
+  wire Z = opcode == 5'b11100;
 
   assign imm = I ? immI :
                S ? immS :
                B ? immB :
                U ? immU :
-               J ? immJ : 'x;
+               J ? immJ :
+               Z ? immZ : 'x;
 
-  assign invalid = !(R | I | S | B| U | J) || inst[1:0] != 2'b11;
+  assign invalid = !(R | I | S | B| U | J | Z) || inst[1:0] != 2'b11;
 endmodule
